d19eb8ee9ab321f7b2057474c5f285cdf6e37df3
Mateusz Kiszka <mateusz.kiszka@komlogo.pl>
2022-05-09 12:00:42 +0200

    Test framework added

---
+++ agent/test/java/com/amirov/jirareporter/ReporterTest.java
@@ -0,0 +1,10 @@
+package com.amirov.jirareporter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReporterTest {
+
+    @org.junit.jupiter.api.Test
+    void transitionIssue() {
+    }
+}
